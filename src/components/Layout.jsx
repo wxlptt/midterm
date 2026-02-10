cc3d69d391ba80df525161bfdd2b78f3138d1148
wxlptt <wxlptt@gmail.com>
2026-02-09 18:55:33 -0800

    new

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
-import AlbumSidebar from "./BookList"
-import AlbumTrack from "./BookTrack";
+import BookList from "./BookList"
+import BookTrack from "./BookTrack";
 
 export default function PlayerLayout( {books, selectedBook} ) {
     return (
         <div style={styles.card}>
-            <AlbumSidebar books={books} selectedBook={selectedBook}/>
-            <AlbumTrack selectedBook={selectedBook} />
+            <BookList books={books} selectedBook={selectedBook}/>
+            <BookTrack selectedBook={selectedBook} />
         </div>
     )
 }
